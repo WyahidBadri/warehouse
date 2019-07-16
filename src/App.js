ff20bccaa18f83dcba9085b34d77bd7c6e628d48
Wyahid Badri <wyahidb2015@gmail.com>
2019-07-16 14:36:36 -0400

    added time

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import TodayPickups_cells from './components/todayPickup_cells'
 import './App.css';
 
 function App() {
-
-  // const [order, setOrder] = useState(0)
-
-  // useEffect(()=>{
-  //   setOrder(1)
-  // },[])
     return (
         <div className="pickup-app">
           <TodayPickups_cells />
